848d304c745091afb9d0996c8f8ef03c9c0e1b93
riba12331 <fedakandrew@gmail.com>
2020-05-24 19:17:57 +0300

    'a'

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-
-app.use(express.json());
-app.use(express.static(path.join(__dirname, './build')));
-
-app.listen(process.env.PORT);
